6e53557d886ff6453a6c113caa6fd214bba78122
Allan Gaigher <gaigher.allan@gmail.com>
2026-03-24 10:59:22 +0000

    Fix TypeError in icon_svg by using explicit width/height parameters

--- app/components.py
+++ app/components.py
@@ -27,12 +27,12 @@ class IC:
     chevron_right = "m9 18 6-6-6-6"
     credit_card = "M2 10h20 M2 6a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V6z"
 
-def icon_svg(d_path, **kwargs):
+def icon_svg(d_path, width="18", height="18", **kwargs):
     if 'style' not in kwargs: kwargs['style'] = "display: block;"
     return Svg(
         NotStr(f'<path d="{d_path}"></path>'),
         xmlns="http://www.w3.org/2000/svg",
-        width="18", height="18", viewBox="0 0 24 24",
+        width=width, height=height, viewBox="0 0 24 24",
         fill="none", stroke="currentColor",
         stroke_width="2", stroke_linecap="round", stroke_linejoin="round",
         **kwargs
